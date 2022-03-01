67cde5d6fb6c704aa25fcb576e8dfef1fded455f
aicoconfs <aicotsu@gmail.com>
2022-03-01 15:47:27 +0800

    增加說明example.py

--- main.py
+++ main.py
@@ -6,15 +6,7 @@ from model import keyword
 
 df = pd.read_csv('選擇題.csv')
 
-# jieba.set_dictionary('dict.txt.big')  # load extra dictionary
 
-# jieba.load_userdict("userdict.txt")  # load user defined dictionary
-# words = jieba.cut(test_string, cut_all=False)  # 使用精確模式
-# out = []
-
-# for word in words:
-    # out.append(word)
-# [list(jieba.cut(i[0], cut_all=False) )for i in  df.values][0]
 
 #%%
 # 模型初始化
